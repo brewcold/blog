2601cf8b086086d152add200a2a2378d5c93fddd
brewcold <seung.yoon.yu@gmail.com>
2026-02-05 12:38:23 +0900

    guestbook 기능 삭제

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -1,11 +1,5 @@
 import { About } from 'features/about'
-import { GuestbookContainer } from 'features/guestbook'
 
 export default async function AboutPage() {
-  return (
-    <>
-      <About />
-      <GuestbookContainer />
-    </>
-  )
+  return <About />
 }
--- features/guestbook/containers/GuestbookContainer.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from '@vanilla-extract/css'
-import { dp } from 'styles/dp'
-
-export const guestbookContainerFrame = style({
-  marginBottom: dp(24),
-})
