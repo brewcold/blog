aaf715bc38ac0a16c0a35aad2dffcdd2e687ca26
brewcold <seung.yoon.yu@gmail.com>
2026-03-16 14:47:43 +0900

    글 메인 스타일 변경

--- app/(routes)/[year]/[slug]/page.css.ts
+++ app/(routes)/[year]/[slug]/page.css.ts
@@ -1,10 +1,14 @@
 import { style } from '@vanilla-extract/css'
+import { layouts } from 'styles/vars/layouts.css'
 
 export const tocBreakpoint = 'screen and (min-width: 768px)'
 
 export const contentLayout = style({
   display: 'flex',
   flexDirection: 'column',
+  maxWidth: layouts.width,
+  width: '100%',
+  marginInline: 'auto',
   '@media': {
     [tocBreakpoint]: {
       flexDirection: 'row',
